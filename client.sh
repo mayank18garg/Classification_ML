c1bc33e14fb83659b01fe034561c798e65f1dd75
mayank18garg <mayank.ind18@gmail.com>
2021-01-25 18:03:34 +0530

    testfile client side

---
+++ client.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo 'Please provide test image path as an argument' 
+    exit 1
+fi
+#result=$(curl -F 'imagefile=@'$1'' http://127.0.0.1:8888/classification --silent)
+result=$(curl -F 'imagefile=@'$1'' http://ec2-13-232-211-181.ap-south-1.compute.amazonaws.com:8888/classification --silent)
+echo "$result"
